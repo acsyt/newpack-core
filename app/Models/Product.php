89dd2dca2af6c1d9c505131eb9bd6815229f3bc8
Gian <giancarlozapata13@gmail.com>
2025-11-25 13:29:48 -0600

    feat: Add comprehensive development stubs and implement initial Product resource.

---
+++ app/Models/Product.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
+
+class Product extends Model
+{
+    use HasFactory, LogsActivity;
+
+    public function getActivityLogOptions(): LogOptions {
+        return LogOptions::defaults()
+            ->logFillable()
+            ->useLogName('Product')
+            ->dontSubmitEmptyLogs()
+            ->logOnlyDirty();
+    }
+}
