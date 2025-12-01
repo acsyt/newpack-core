44a7e94095b3e74b2d7ee8552aebffca69767d00
Gian <giancarlozapata13@gmail.com>
2025-12-01 09:27:04 -0600

    feat: Return customer resource as a JSON response in `findOneCustomer`.

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -87,7 +87,7 @@ class CustomerController extends Controller
     public function findOneCustomer($id)
     {
         $customer = CustomerQuery::make()->findById((int) $id);
-        return new CustomerResource($customer);
+        return response()->json(new CustomerResource($customer));
     }
 
     /**
